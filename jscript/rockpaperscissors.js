8f674b82352a237b70777e77a414c963b21b0a62
BvB-tech <jacktechfora@proton.me>
2023-08-27 20:12:40 +0200

    zondag

--- jscript/rockpaperscissors.js
+++ jscript/rockpaperscissors.js
@@ -16,102 +16,121 @@ let playerSelection;
 let computerSelection;
 let element;
 let result;
+let finished = false;
 // function processClick() {
 //   window.playerSelection = this.id;
 // }
 
-const choose = document.querySelectorAll(".choose button");
-choose.forEach((choose) => {
-  choose.addEventListener("click", processClick);
-});
-
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
+  computerSelection = "start";
   let randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
+
 computerSelection = getComputerChoice();
 
 function processClick() {
+  finished = false;
+
   window.playerSelection = this.id;
   switch (window.playerSelection) {
     case "rock" && computerSelection === "rock":
       result = document.getElementById("matchOutcome").innerHTML = "It's a Tie";
+
       break;
     case "rock" && computerSelection === "paper":
       result = document.getElementById("matchOutcome").innerHTML = "you loose";
       break;
     case "rock" && computerSelection === "scissors":
       result = document.getElementById("matchOutcome").innerHTML = "you win";
+
       break;
     case "paper" && computerSelection === "rock":
       result = document.getElementById("matchOutcome").innerHTML = "you win";
+
       break;
     case "paper" && computerSelection === "paper":
       result = document.getElementById("matchOutcome").innerHTML = "It's a Tie";
+
       break;
     case "paper" && computerSelection === "scissors":
       result = document.getElementById("matchOutcome").innerHTML = "you loose";
+
       break;
     case "scissors" && computerSelection === "rock":
       result = document.getElementById("matchOutcome").innerHTML = "you loose";
+      return location.reload();
       break;
     case "scissors" && computerSelection === "paper":
       result = document.getElementById("matchOutcome").innerHTML = "you win";
+
       break;
     case "scissors" && computerSelection === "scissors":
       result = document.getElementById("matchOutcome").innerHTML = "It's a Tie";
+
       break;
   }
-
-  // function playRound(playerSelection, computerSelection) {
-  //   computerSelection = getComputerChoice();
-  // document.addEventListener("click", function (evt) {
-  //   element = evt.target;
-  //   playerSelection = element + document.getElementById("rock");
-
-  // if (playerSelection === "paper" && computerSelection === "paper") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "It's a tie";
-  // } else if (playerSelection === "rock" && computerSelection === "rock") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "It's a tie";
-  // } else if (
-  //   playerSelection === "scissors" &&
-  //   computerSelection === "scissors"
-  // ) {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "It's a tie";
-  // } else if (playerSelection === "paper" && computerSelection === "rock") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "you win congrats");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "you win congrats";
-  // } else if (playerSelection === "paper" && computerSelection === "scissors") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "You losse, better luck next time";
-  // } else if (playerSelection === "scissors" && computerSelection === "paper") {
-  //   // console.log("you win congrats");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "you win congrats";
-  // } else if (playerSelection === "scissors " && computerSelection === "rock") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "You losse, better luck next time";
-  // } else if (playerSelection === "rock" && computerSelection === "paper") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "You losse, better luck next time";
-  // } else if (playerSelection === "rock" && computerSelection === "scissors") {
-  //   // console.log("The computer chose" + " " + computerSelection + " " + "you win congrats");
-  //   result = document.getElementById("matchOutcome");
-  //   result.textContent = "You win congrats";
-  // }
 }
+
+const choose = document.querySelectorAll(".choose button");
+choose.forEach((choose) => {
+  finished = false;
+  choose.addEventListener("click", processClick);
+  return (finished = true);
+});
+
+if (!finished) {
+  getComputerChoice();
+}
+
+// function playRound(playerSelection, computerSelection) {
+//   computerSelection = getComputerChoice();
+// document.addEventListener("click", function (evt) {
+//   element = evt.target;
+//   playerSelection = element + document.getElementById("rock");
+
+// if (playerSelection === "paper" && computerSelection === "paper") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "It's a tie";
+// } else if (playerSelection === "rock" && computerSelection === "rock") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "It's a tie";
+// } else if (
+//   playerSelection === "scissors" &&
+//   computerSelection === "scissors"
+// ) {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "It's a tie");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "It's a tie";
+// } else if (playerSelection === "paper" && computerSelection === "rock") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "you win congrats");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "you win congrats";
+// } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "You losse, better luck next time";
+// } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//   // console.log("you win congrats");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "you win congrats";
+// } else if (playerSelection === "scissors " && computerSelection === "rock") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "You losse, better luck next time";
+// } else if (playerSelection === "rock" && computerSelection === "paper") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "you loose better luck next time");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "You losse, better luck next time";
+// } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//   // console.log("The computer chose" + " " + computerSelection + " " + "you win congrats");
+//   result = document.getElementById("matchOutcome");
+//   result.textContent = "You win congrats";
+// }
 
 // console.log(`Clicked button: ${element.textContent}`);
 // playerSelection = document.getElementById("rock");
